ff57f66c501d7e3aa209fa67fc653d1f3452ff4f
akashflockngo <akash@flockngo.com>
2026-05-07 12:00:17 +0530

    Added basic dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
-    <div>
-      <h1 className="">FlocknGo Web Portal</h1>
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">FlocknGo Web Portal</h1>
     </div>
   );
 };
