9547ce649d4b6f267a7249182edde15ef6cb4794
akashflockngo <akash@flockngo.com>
2026-05-24 02:00:04 +0530

    Create reusable PageHeader component and update AllFlocks and AllActivities pages

--- src/modules/listing/pages/activities/AllActivities.tsx
+++ src/modules/listing/pages/activities/AllActivities.tsx
@@ -1,9 +1,8 @@
-import { Link, useNavigate, useParams } from "react-router-dom";
-import { Icons } from "../../../../constants/icons";
+import { Link, useParams } from "react-router-dom";
 import NearbyActivities from "../../components/home/NearbyActivities";
 import { useEffect, useState, useRef } from "react";
 import HomeLoader from "../../../../components/common/HomeLoader";
-import TitleText from "../../../../components/common/TitleText";
+import PageHeader from "../../../../components/common/PageHeader";
 import ScrollLoader from "../../../../components/common/ScrollLoader";
 import { useAppDispatch, useAppSelector } from "../../../../store/hooks";
 import { fetchActivitiesPage } from "../../../../store/slices/activitySlice";
@@ -11,7 +10,6 @@ import { fetchActivitiesPage } from "../../../../store/slices/activitySlice";
 import ErrorState from "../../../../components/common/ErrorState";
 
 const AllActivities = () => {
-  const navigate = useNavigate();
   const { search_by } = useParams();
   const dispatch = useAppDispatch();
   const { activities, loading, page, hasMore, error } = useAppSelector((state) => state.activities);
@@ -83,28 +81,7 @@ const AllActivities = () => {
     <main className="min-h-screen px-4  sm:px-6 md:px-8 lg:px-12 xl:px-16 flex flex-col gap-16 py-10">
       {/* Nearby Flocks */}
       <section className="">
-        {/* Heading */}
-        <div className="flex justify-between items-center mb-4">
-          <div className="">
-            <TitleText
-              title={`${search_by
-                ?.split("-")
-                .map(
-                  (word) =>
-                    word.charAt(0).toUpperCase() + word.slice(1).toLowerCase(),
-                )
-                .join(" ")}`}
-            />
-          </div>
-          <div className="">
-            <button
-              onClick={() => navigate(-1)}
-              className="bg-linear-to-tr from-btn02 to-btn01 to-75% px-5 py-2 bg-clip-text text-transparent transition-all duration-300 hover:scale-105 flex items-center gap-2 cursor-pointer"
-            >
-              <Icons.leftArrow className="text-btn01" /> Back
-            </button>
-          </div>
-        </div>
+        <PageHeader slug={search_by} />
 
         {/* Activities List */}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 gap-x-4 gap-y-16">
--- src/modules/listing/pages/flocks/AllFlocks.tsx
+++ src/modules/listing/pages/flocks/AllFlocks.tsx
@@ -1,7 +1,6 @@
-import { Link, useNavigate, useParams } from "react-router-dom";
-import { Icons } from "../../../../constants/icons";
+import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import TitleText from "../../../../components/common/TitleText";
+import PageHeader from "../../../../components/common/PageHeader";
 import { useAppDispatch, useAppSelector } from "../../../../store/hooks";
 import { fetchFlocksPage } from "../../../../store/slices/flockSlice";
 import NearbyFlock from "../../components/home/NearbyFlock";
@@ -12,7 +11,6 @@ import ErrorState from "../../../../components/common/ErrorState";
 import HomeLoader from "../../../../components/common/HomeLoader";
 
 const AllFlocks = () => {
-  const navigate = useNavigate();
   const { search_by } = useParams();
   const { flocks, hasMore, loading, error } = useAppSelector((state) => state.flock);
   const [page, setPage] = useState(1);
@@ -65,29 +63,7 @@ const AllFlocks = () => {
     <main className="h-screen px-4  sm:px-6 md:px-8 lg:px-12 xl:px-16 flex flex-col gap-16 py-10">
       {/* Nearby Flocks */}
       <section className="">
-        {/* Heading */}
-        <div className="flex justify-between items-center mb-4">
-          <div className="">
-            <TitleText
-              title={`
-              ${search_by
-                ?.split("-")
-                .map(
-                  (word) =>
-                    word.charAt(0).toUpperCase() + word.slice(1).toLowerCase(),
-                )
-                .join(" ")}`}
-            />
-          </div>
-          <div className="">
-            <button
-              onClick={() => navigate(-1)}
-              className="bg-linear-to-tr from-btn02 to-btn01 to-75% px-5 py-2 bg-clip-text text-transparent transition-all duration-300 hover:scale-105 flex items-center gap-2 cursor-pointer"
-            >
-              <Icons.leftArrow className="text-btn01" /> Back
-            </button>
-          </div>
-        </div>
+        <PageHeader slug={search_by} />
 
         {/* Activities List */}
         <InfiniteScroll
